ccb4f29adc4555ba8b32e0a47ca1ee07cc63ec06
Dapijon <144019972+Dapijon@users.noreply.github.com>
2023-10-03 13:03:22 +0300

    Update Gradegenerator.js

--- sol1/Gradegenerator.js
+++ sol1/Gradegenerator.js
@@ -1,13 +1,14 @@
+//function get student grade based on marks
 function getStudentGrade() {
 
     let mark;
-    
+    //prompt user to enter valid grade
     do {
       mark = parseInt(prompt('Enter student mark (0-100): '));
     } while (isNaN(mark) || mark < 0 || mark > 100);
-  
+  // have the variable to store the determined grade and determine the grade based on the mark using an if-else ladder
     let grade;
-  
+
     if (mark > 79) {
       grade = 'A';
     } else if (mark >= 60) {
@@ -19,7 +20,7 @@ function getStudentGrade() {
     } else {
       grade = 'E';
     }
-  
+  //return grade
     return grade;
   
   }
